f41abe09119534e4313adf1288fbcb624fb397d5
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-02 20:05:41 -0500

    article component created

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  @HostBinding('attr.class') cssClass = 'card container';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'https://angular.io';
+    this.votes = 10;
+  }
+
+  ngOnInit(): void {
+  }
+
+  voteUp(): boolean {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.votes -= 1;
+    return false;
+  }
+
+}
